0514ee25e12e59020f8fc9d58e49ba1a3cf0c878
Anastasia Kukuyan <58336918+kknnsts@users.noreply.github.com>
2020-10-23 16:21:20 +0300

    changes

--- library/check.php
+++ library/check.php
@@ -1,10 +1,12 @@
 <?php
 	$surname = filter_var(trim($_POST['surname']), FILTER_SANITIZE_STRING);
 	$name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
-	$father = filter_var(trim($_POST['father']), FILTER_SANITIZE_STRING);
 	$tel = filter_var(trim($_POST['tel']), FILTER_SANITIZE_STRING);
-	$email = filter_var(trim($_POST['email']), FILTER_SANITIZE_STRING);
-	$event = filter_var(trim($_POST['event']), FILTER_SANITIZE_STRING);
+	$book = filter_var(trim($_POST['book']), FILTER_SANITIZE_STRING);
+	$date_start = $_POST['start'];
+	$date_finish = $_POST['finish'];
+	$reader = filter_var(trim($_POST['reader']), FILTER_SANITIZE_STRING);
+	
 	
 	if(mb_strlen($surname)<1 || mb_strlen($surname)>50){
 		echo '<div style="color: red;">Неправильно введене прізвище!</div><hr>';
@@ -20,17 +22,13 @@
 		exit();
 	}
 	
-	else if(strpos($email, '@')== false){
-		echo '<div style="color: red;">Неправильно введений email!</div><hr>';
-		exit();
-	}
+	
 	
 	$mysql = new mysqli('library','mysql','mysql','library');
-	$mysql->query("INSERT INTO `users` (`surname`, `name`,
-	`fath`, `number`, `email`, `event`) VALUES('$surname', '$name', 
-	'$father', '$tel', '$email', '$event')");
+	$mysql->query("INSERT INTO `users` (`surname`, `name`, `number`, `book`, `start`, `finish`, `answer`) VALUES('$surname', '$name', 
+	'$tel', '$book', '$date_start', '$date_finish', '$reader')");
 	
-	print_r('<div style="color: green;">Ви успішно зареєструвались!</div><hr>');
+	print_r('<div style="color: green;">Ви успішно забронювали книгу!</div><hr>');
 	exit();
 	
 	$mysql->close();
